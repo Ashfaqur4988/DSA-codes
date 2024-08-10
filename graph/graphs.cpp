a7a85b25db8c8191306d1e174fcf9b56dec41165
Ashfaqur Riaz <ashfakurriaz@gmail.com>
2024-08-10 20:25:12 +0530

    graph start

---
+++ graph/graphs.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    // adjacency matrix
+    int n, m;
+    cin >> n >> m;
+    // graph (adjacency matrix)
+    int adj[n + 1][m + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+    // graph (adjacency matrix)
+
+    // graph list store
+    vector<int> vec[n + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        vec[u].push_back(v);
+        vec[v].push_back(u);
+    }
+    // graph list store
+
+    // weighted graph list store
+    vector<int> weighted[n + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        // only one link
+        weighted[u].push_back(v);
+    }
+
+    return 0;
+}
