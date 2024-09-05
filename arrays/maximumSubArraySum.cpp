321ed645d806c70cda8e68bd639f184e115732d2
Ashfaqur Riaz <ashfakurriaz@gmail.com>
2024-09-06 02:24:18 +0530

    kadanes Algo

---
+++ arrays/maximumSubArraySum.cpp
@@ -0,0 +1,23 @@
+// kadanes algo
+int maxSubArray(vector<int> &nums)
+{
+    int maxi = INT_MIN;
+    int sum = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sum = sum + nums[i];
+
+        if (sum > maxi)
+        {
+            maxi = sum;
+        }
+        // maxi = max(maxi, sum);
+
+        if (sum < 0)
+        {
+            sum = 0;
+        }
+    }
+    // if(maxi < 0) return 0;
+    return maxi;
+}
